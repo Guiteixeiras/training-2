c065a996eef91d4dc7d65650e1396574bd8a73b6
murilomafra1302 <guilherme.tsilva03@gmail.com>
2022-10-31 16:59:28 -0400

    att

---
+++ php/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <input type="number" name="valor1">
+
+    <input type="number" name="valor2">
+
+    <?php
+
+    echo "Calculadora<br>";
+
+    $valor1 = readline('Digite o primeiro numero: ');
+    echo "\n";
+
+    $valor2 = readline('Digite o segundo numero: ');
+    echo "\n";
+
+    $soma = ("$valor1" + "$valor2");
+    $div = ("$valor1" / "$valor2");
+    $mult = ("$valor1" * "$valor2");
+    $sub = ("$valor1" - "$valor2");
+
+    echo "Sua soma da $soma \n";
+    echo "Sua divisão da $div \n";
+    echo "Sua multiplicação da $mult \n";
+    echo "Sua subtração da $sub \n";
+
+    function writemsg() {
+        echo 'DEU CERTOO';
+    }
+
+    writemsg();
+
+    ?>
+</body>
+
+</html>
---
+++ tentando-muito/2index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscrição</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  
+    <?php
+    $nome = $_GET["nome"];
+    $sobrenome = $_GET["sobrenome"];
+    $ano = $_GET["idade"];
+    $sexo = $_GET["sexo"];
+    
+    $idade = date("Y") - $ano;
+    
+    echo "$nome é do sexo $sexo e tem $ano anos"
+    
+    ?>
+</body>
+
+</html>
