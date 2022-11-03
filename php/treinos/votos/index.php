169fa69d3bdf8dfbdadbe9588b2392d5bd3f5ef2
murilomafra1302 <guilherme.tsilva03@gmail.com>
2022-11-03 15:21:31 -0400

    att.2

--- php/treinos/votos/index.php
+++ php/treinos/votos/index.php
@@ -1,5 +1,28 @@
 <?php
 
+$nota1 = $_GET["nota1"];
+$nota2 = $_GET["nota2"];
+
+$notaf = ("$nota1" + "$nota2");
+$media = ("$notaf" / 2);
+
+if ($media <= 5) {
+    $a = "esta reprovado";
+} else {
+    if ($media >= 6 && $media < 7) {
+        $a = "esta de recuperação";
+    } else {
+        $a = "esta aprovado";
+    }
+}
+
+echo "sua primeira nota foi $nota1 ja a segunda foi $nota2 fazendo com que sua média fosse $media \n";
+echo " e com esta media o aluno $a";
+
+
+//ver se pode dirigr e votar
+
+/*
 $idade = $_GET["idade"];
 
 if ($idade>=18) {
@@ -11,3 +34,4 @@ if ($idade>=18) {
 }
 
 echo "com esta idade você $a e também $b :)";
+*/
